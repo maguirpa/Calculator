9e33d9f27b496ebde86fd0119a11e00ed1165120
Patrick Maguire <maguirpa@gmail.com>
2015-02-25 23:35:06 -0500

    Calculator updates

--- calculator.rb
+++ calculator.rb
@@ -1,30 +1,39 @@
+def say(message)
+  puts "=> #{message}"
+end
+
+begin
 begin
 
-puts "==> Please pick the first number of the equation."
+say "Please pick the first number of the equation."
 num1 = gets.chomp
 
-puts "==> Please pick the second number of the equation."
+say "Please pick the second number of the equation."
 num2 = gets.chomp
 
-puts "==> Would you like to 1) add, 2) subtract, 3) multiply or 4) divide?"
+say "Would you like to 1) Add, 2) Subtract, 3) Multiply or 4) Divide?"
 action = gets.chomp
-restart = ' '
+restart = ' '  
 
 case 
 when action == '1'
-  puts "==> The result is #{num1.to_i + num2.to_i}"
+  say "The result is #{num1.to_i + num2.to_i}"
 when action == '2'
-  puts "==> The result is #{num1.to_i - num2.to_i}"
+  say "The result is #{num1.to_i - num2.to_i}"
 when action == '3'
-  puts  puts "==> The result is #{num1.to_i * num2.to_i}"
+  say "The result is #{num1.to_i * num2.to_i}"
 when action == '4'
-  puts "==> The result is #{num1.to_f / num2.to_f}"
+  say "The result is #{num1.to_f / num2.to_f}"
 else
-  puts "There was an error, please try again."
+  say "There was an error, please try again."
   restart = 'y' 
 end
-end while restart == 'y'
+end while restart == 'y'  # loop until player inputs valid operator. 
 
+puts "Would like like to exit the calculator? (y/n)"
+answer = gets.chomp
+
+end while answer != 'y'
 
 
 
